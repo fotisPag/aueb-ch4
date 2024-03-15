d2cdafa9683877573ccd83505f76f3ca84815f63
fotisPag <fotis.pagonis01@gmail.com>
2024-03-15 19:22:00 +0200

    chapter 4

---
+++ DeclarationApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.ch4;
+
+public class DeclarationApp {
+
+    public static void main(String[] args) {
+        final char EMPTY_CHAIR = ' ';
+        char star = '*';
+        char cha = 'A';
+    }
+
+}
